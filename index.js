b90b25b4b416e4c5e1a167773b7d418487239908
Yash Gupta <itsyashgupta.18@gmail.com>
2023-10-15 22:14:59 +0530

    premium proxy added

--- index.js
+++ index.js
@@ -13,22 +13,25 @@ const port = 5000;
 let token = null;
 let tokenExpirationTime = null;
 
-app.get('/', (req, res) => {
+app.get('/udaan', (req, res) => {
+  console.log("req rec");
 
-  let url = 'https://corsproxy.io/?https%3A%2F%2Fudaan.com%2Fapi%2FfoodFmcgSearch%2Fv1%2Flisting%2FTLFRC4BJF4F42RCQ6CGBMFB347WBRY8=';
-
-  let options = {method: 'GET'};
-  
-  fetch('https://api.scraperapi.com/?api_key=b7fe6fcbc069576c789172a800bbf76f&url=https%3A%2F%2Fudaan.com%2Fapi%2FfoodFmcgSearch%2Fv1%2Flisting%2FTLFRC4BJF4F42RCQ6CGBMFB347WBRY8')
-  .then(response => {
-    console.log(response)
-    res.json(response)
-  })
-  .catch(error => {
-    console.log(error)
-    res.json(error)
-
-  });
+  axios.get('https://app.scrapingbee.com/api/v1', {
+    params: {
+        'api_key': 'PHSQVQ8M31S98WS6LZJ36Z7SBXG8Z5BZFR41KD15EOHQGXKYJMVNKTD1MKQB5CETFL12QSKW5GOC0ZYD',
+        'url': 'https://udaan.com/api/foodFmcgSearch/v1/listing/TLFRC4BJF4F42RCQ6CGBMFB347WBRY8',  
+        'premium_proxy': 'true',
+        'stealth_proxy': 'true', 
+        'country_code':'in'
+    } 
+}).then(function (response) {
+    // handle success
+    console.log(response);
+    res.send(response.data)
+}).catch(e => {
+  console.log(e);
+  res.send(e.data);
+})
 
  
 });
