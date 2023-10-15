980856f6f8296cb65700502880006de40195f434
Yash Gupta <itsyashgupta.18@gmail.com>
2023-10-15 23:45:08 +0530

    proxy addde

--- index.js
+++ index.js
@@ -16,18 +16,14 @@ let tokenExpirationTime = null;
 app.get('/udaan', (req, res) => {
   console.log("req rec");
 
-  axios.get('https://app.scrapingbee.com/api/v1', {
-    params: {
-        'api_key': 'PHSQVQ8M31S98WS6LZJ36Z7SBXG8Z5BZFR41KD15EOHQGXKYJMVNKTD1MKQB5CETFL12QSKW5GOC0ZYD',
-        'url': 'https://udaan.com/api/foodFmcgSearch/v1/listing/TLFRC4BJF4F42RCQ6CGBMFB347WBRY8',  
-        'premium_proxy': 'true',
-        'stealth_proxy': 'true', 
-        'country_code':'in'
-    } 
+  axios.get('https://corsproxy.org/?https://udaan.com/api/foodFmcgSearch/v1/listing/TLFRC4BJF4F42RCQ6CGBMFB347WBRY8', {
+    headers: {
+      Authorization: 'Bearer eyJraWQiOiI2TW53IiwidHlwIjoiSldUIiwiYWxnIjoiSFM1MTIifQ.eyJhdWQiOiJodHRwczovL2FwaS51ZGFhbi5jb20vIiwic3ViIjoiVVNSMExKVjRENFcwVERSQjRNTlpINjZISDlaTDAiLCJyIjpbInVzZXIiXSwibmJmIjoxNjk3MzkzMTEwLCJpc3MiOiJhdXRoLnVkYWFuLmNvbSIsImV4cCI6MTY5NzM5MzcxMCwibyI6Ik9SRzFCOEVIRkpLMkZDN0RGS0xMUjJFOENaTkYwIn0.NsCTVdNRz_ie5JNCACG_IHy9-6Ey4GZ2gE-gJVdzKcUM2JhDWS2SI6ikOmcz0_Sy42KBWBtNyayQ1kIe9mNoXg'
+    }
 }).then(function (response) {
     // handle success
     console.log(response);
-    res.send(response.data)
+    res.json(response.data)
 }).catch(e => {
   console.log(e);
   res.send(e.data);
