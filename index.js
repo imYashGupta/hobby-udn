fe4ab4ac2689f505d8c562d0b528f23080846aef
Yash Gupta <itsyashgupta.18@gmail.com>
2023-10-15 18:50:12 +0530

    scrapperAPI

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-const fs = require('fs');
-const express = require('express');
-const axios = require('axios');
-const qs = require('qs');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+import fetch from 'node-fetch';
+import fs from 'fs';
+import express from 'express';
+import axios from 'axios';
+import qs from 'qs';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+
 
 const app = express();
 const port = 5000;
 
 let token = null;
 let tokenExpirationTime = null;
-//http://localhost:5000/product?id=TLFSRZE6X0NP8FXQDJZC58VJ5LWQNM9
-// Function to read the access token from a file
-app.use('/api', createProxyMiddleware({ target: 'https://api.udaan.com', changeOrigin: true }));
 
+app.get('/', (req, res) => {
+
+  let url = 'https://corsproxy.io/?https%3A%2F%2Fudaan.com%2Fapi%2FfoodFmcgSearch%2Fv1%2Flisting%2FTLFRC4BJF4F42RCQ6CGBMFB347WBRY8=';
+
+  let options = {method: 'GET'};
+  
+  fetch('https://api.scraperapi.com/?api_key=b7fe6fcbc069576c789172a800bbf76f&url=https%3A%2F%2Fudaan.com%2Fapi%2FfoodFmcgSearch%2Fv1%2Flisting%2FTLFRC4BJF4F42RCQ6CGBMFB347WBRY8')
+  .then(response => {
+    console.log(response)
+    res.json(response)
+  })
+  .catch(error => {
+    console.log(error)
+    res.json(error)
+
+  });
+
+ 
+});
 
 app.listen(port, () => console.log(`Proxy server running on port ${port}`));
 
