b20c5924bcb5ba74b5f14b6c71a2a311bae9e0ba
Yash Gupta <itsyashgupta.18@gmail.com>
2023-10-19 15:14:35 +0530

    puppeteer added

--- index.js
+++ index.js
@@ -7,151 +7,18 @@ const fs = require('fs');
 const express = require('express');
 const axios = require('axios');
 const qs = require('qs');
- 
+const cors = require('cors');
+const {scrapper} = require("./scrapper");
 
 const app = express();
-const port = 5000;
+app.use(cors());
 
-let token = null;
-let tokenExpirationTime = null;
+const PORT = process.env.PORT || 5000;
+ 
 
-
-function parseJwt (token) {
-  return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
-}
-
-function readAccessToken() {
-    
-    try {
-      const accessToken = fs.readFileSync('access_token.txt', 'utf8');
-      if (accessToken.trim() !== '') {
-        let fileData = JSON.parse(accessToken);
-        token = fileData.token;
-        tokenExpirationTime = fileData.expirationTime;
-        console.log('Access token loaded:', fileData);
-      } else {
-        console.log('Access token file is found empty. getting a new token');
-        getAccessToken();
-      }
-    } catch (error) {
-      console.error('Error reading access token:', error);
-    }
-  }
-
-// Function to write the access token to a file
-function writeAccessToken() {
-  try {
-    const data = JSON.stringify({ token, expirationTime: tokenExpirationTime });
-    fs.writeFileSync('access_token.txt', data, 'utf8');
-    console.log('Access token saved to file:', token);
-  } catch (error) {
-    console.error('Error writing access token:', error);
-  }
-}
-
-// Function to make the initial request to get the access token
-function getAccessToken() {
-  const options = {
-    method: 'POST',
-    url: 'https://corsproxy.org/',
-    params: {'https://auth.udaan.com/app/v1/refresh?cid': 'udaan-v2'},
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    data: qs.stringify({
-      rt: 'URdtZXYYz3EBACAWQghQu7Em9S3e8wtngzq7F4LEhcseKCN/kfvk6k2ErF3LWQ32VZD8Gc9xJCQzVDFXX1n2tpsXzMvK3h3EhmhuqiUkdBH7C6wpV23WkddPWSu4cnQjsruRy/S/bi+uxHEpdwkTSiQICxwNPShQ24m00jAc7Yq12W8XMd9hOTFmMzY2Nw=='
-    })
-  };
-
-  return axios.request(options)
-    .then(function (response) {
-      token = response.data.accessToken;
-      const expiresIn = response.data.expiresIn;
-      tokenExpirationTime = Date.now() + (expiresIn * 1000); // Convert expiresIn to milliseconds and set the token expiration time
-      console.log('Access token obtained:', token);
-      console.log('Token expiration time:', new Date(tokenExpirationTime));
-      writeAccessToken(); // Save the access token to a file
-    })
-    .catch(function (error) {
-      console.error('Error getting access token:', error);
-    });
-}
-
-// Function to make the request to get the product details
-function getProductDetails(prodID) {
-  return refreshTokenIfNeeded()
-    .then(() => {
-      console.log("my token", token); // Log the token after it has been obtained
-      const options = {
-        method: 'GET',
-        url: 'https://corsproxy.org/?https://api.udaan.com/api/foodFmcgSearch/v1/listing/'+prodID,
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      };
-
-      return axios.request(options)
-        .then(function (response) {
-        //   console.log('Product details:', response.data);
-          return response.data; // Return the product details
-        })
-        .catch(function (error) {
-          console.error('Error getting product details:', error);
-          throw error; // Throw the error to be caught later
-        });
-    });
-}
-
-// Function to check if the token has expired
-function isTokenExpired() {
-  return tokenExpirationTime && Date.now() >= tokenExpirationTime;
-}
-
-// Function to refresh the token if it has expired
-function refreshTokenIfNeeded() {
-  if (isTokenExpired()) {
-    console.log('Token expired, refreshing...');
-    return getAccessToken();
-  } else {
-    console.log('Token still valid, no need to refresh.');
-    return Promise.resolve(); // Return a resolved promise if the token is still valid
-  }
-}
-
-// Load the access token from file when the code starts
-readAccessToken();
-
-// Express.js route to handle the request
-/* app.get('/product', (req, res) => {
-  let prodID = req.query.id;
-  refreshTokenIfNeeded()
-    .then(getProductDetails)
-    .then((productDetails) => {
-      res.json({ productDetails }); // Send the product details as a JSON response
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      res.status(500).send('An error occurred');
-    });
-});  */ 
-
-app.get('/product', (req, res) => {
-    const productID = req.query.id; // Get the product ID from the query parameter
-    console.log(token)
-    if (!productID) {
-      res.status(400).send('Product ID is missing');
-      return;
-    }
-    refreshTokenIfNeeded()
-      .then(() => getProductDetails(productID))
-      .then((productDetails) => {
-        res.json({ productDetails }); // Send the product details as a JSON response
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-        res.status(500).send('An error occurred');
-      });
-  });
+app.get('/scrapper', (req, res) => {
+  scrapper(res);
+});
 
 // Start the Express.js server
-app.listen(port);
+app.listen(PORT);
---
+++ scrapper.js
@@ -0,0 +1,42 @@
+const puppeteer = require("puppeteer");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const scrapper = async (res) => {
+    // Create browser instance, and give it a first tab
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // Allows you to intercept a request; must appear before
+    // your first page.goto()
+    await page.setRequestInterception(true);
+
+    // Request intercept handler... will be triggered with 
+    // each page.goto() statement
+    page.on('request', interceptedRequest => {
+
+        // Here, is where you change the request method and 
+        // add your post data
+        var data = {
+            'method': 'GET',
+            // 'postData': 'paramFoo=valueBar&paramThis=valueThat'
+            headers: {
+                Authorization: 'Bearer eyJraWQiOiI2TW53IiwidHlwIjoiSldUIiwiYWxnIjoiSFM1MTIifQ.eyJhdWQiOiJodHRwczovL2FwaS51ZGFhbi5jb20vIiwic3ViIjoiVVNSMExKVjRENFcwVERSQjRNTlpINjZISDlaTDAiLCJyIjpbInVzZXIiXSwibmJmIjoxNjk3NzA4NDgyLCJpc3MiOiJhdXRoLnVkYWFuLmNvbSIsImV4cCI6MTY5NzcwOTA4MiwibyI6Ik9SRzFCOEVIRkpLMkZDN0RGS0xMUjJFOENaTkYwIn0.KhMR_jXkyy7H_itjCYHW6dxgFi0hvZfOVobqWUZWu-JQxANMgh1H8CQ0NU-OmPpcrd6LZWXI08KWIM09AhFELA'
+              }
+        };
+
+        // Request modified... finish sending! 
+        interceptedRequest.continue(data);
+    });
+
+    // Navigate, trigger the intercept, and resolve the response
+    const response = await page.goto('https://corsproxy.org/?https://udaan.com/api/foodFmcgSearch/v1/listing/TLFRC4BJF4F42RCQ6CGBMFB347WBRY8');
+    const responseBody = await response.json();
+    console.log(responseBody);
+
+    // Close the browser - done! 
+    await browser.close();
+    res.json(responseBody);
+    //   await browser.close();
+}
+
+module.exports = { scrapper };
