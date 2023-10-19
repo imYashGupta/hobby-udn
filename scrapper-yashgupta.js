278b69c89dcdea916833cf8390f00d94409bd1dc
Yash Gupta <itsyashgupta.18@gmail.com>
2023-10-19 22:49:17 +0530

    more twiks

--- scrapper-yashgupta.js
+++ scrapper-yashgupta.js
@@ -1,48 +1,42 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 require("dotenv").config();
- 
+const pluginStealth = require("puppeteer-extra-plugin-stealth"); 
+const { executablePath } = require("puppeteer"); 
+puppeteer.use(pluginStealth()); 
+
 
 
 
 const scrapperYashGupta = async (res) => {
-    // Create browser instance, and give it a first tab
-    const browser = await puppeteer.launch({
-      args: [
-        "--disable-setuid-sandbox",
-        "--no-sandbox",
-        "--single-process",
-        "--no-zygote",
-        "--disable-web-security",
-        "--disable-features=IsolateOrigins",
-        "--disable-site-isolation-trials"
-      ],
-      executablePath:
-        process.env.NODE_ENV === "production"
-          ? process.env.PUPPETEER_EXECUTABLE_PATH
-          : puppeteer.executablePath(),
+  puppeteer.launch({ executablePath: executablePath(),headless:false }).then(async browser => { 
+    // Create a new page 
+    const page = await browser.newPage(); 
+   
+    // Setting page view 
+    await page.setViewport({ width: 1280, height: 720 }); 
+   
+    // Go to the website 
+    await page.goto('https://udaan.yashgupta.work/app.html');
+
+    // Wait for the data to be present after the AJAX call
+    await page.waitForFunction(() => {
+      return document.body.textContent.trim().startsWith('{');
     });
-    const page = await browser.newPage();
-
-     // Navigate to the page with the AJAX request
-  await page.goto('https://udaan.yashgupta.work/app.html');
-
-  // Wait for the data to be present after the AJAX call
-  await page.waitForFunction(() => {
-    return document.body.textContent.trim().startsWith('{');
+  
+    // Extract the data from the page
+    const data = await page.evaluate(() => {
+      return JSON.parse(document.body.textContent);
+    });
+   
+    console.log(data);
+   
+    // Close the browser 
+    await browser.close(); 
   });
 
-  // Extract the data from the page
-  const data = await page.evaluate(() => {
-    return JSON.parse(document.body.textContent);
-  });
-
-  // Print the extracted data
-  console.log(data);
-
-  // Close the browser
-  await browser.close();
+ 
 }
 
 module.exports = { scrapperYashGupta };
