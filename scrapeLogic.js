f75b49c2a9ff86b97ff956af48ee64ba7b4392dc
Yash Gupta <itsYashGupta.18@gmail.com>
2024-02-08 20:10:47 +0530

    stealth added

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,6 +1,8 @@
-const puppeteer = require("puppeteer");
+// const puppeteer = require("puppeteer");
 require("dotenv").config();
-
+const puppeteer = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 const scrapeLogic = async (res) => {
   const browser = await puppeteer.launch({
     headless:true,
@@ -20,9 +22,10 @@ const scrapeLogic = async (res) => {
     await page.goto('https://www.myntra.com/',{waitUntil:'domcontentloaded'});
     await page.waitForTimeout(2000);
     const response = await page.goto('https://www.myntra.com/gateway/v2/product/16354886');
+    console.error(response);
 
     const responseBody = await response.json();
-     
+    
     res.send(responseBody);
   } catch (e) {
     console.error(e);
